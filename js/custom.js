6bba189aad7b4ed090d151e057344d9ceb2a34f7
elina <elina.prints@gmail.com>
2021-10-21 00:26:42 +0300

    landing

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+    let bg = document.getElementById("bg");
+    setInterval(changeBg(), 3000);
+    changeBg => { for(let i=1;i <= 3;i++){
+        
+    }};
+
+
+
+
+});
